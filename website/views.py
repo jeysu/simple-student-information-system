c976944837fb09982de23f2aebce84eb960a0ad3
jacevihzalel <jacevihzalel@gmail.com>
2024-11-07 05:20:54 +0800

    Added search bar and created base html template where student and course pages extend from

--- website/views.py
+++ website/views.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 views = Blueprint('route', __name__)
 
 @views.route('/')
-def home():
-    return render_template("base.html")
+def students():
+    return render_template("students.html")
