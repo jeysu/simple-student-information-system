5d2ea9016b74c37e8b7edcc7590646f6d75e08f2
jacevihzalel <jacevihzalel@gmail.com>
2024-11-08 04:37:04 +0800

    Added form input modal for New Student

--- website/views.py
+++ website/views.py
@@ -2,6 +2,10 @@ from flask import Blueprint, render_template
 
 views = Blueprint('route', __name__)
 
-@views.route('/')
+@views.route('')
 def students():
     return render_template("students.html")
+
+@views.route('courses')
+def courses():
+    return render_template("courses.html")
