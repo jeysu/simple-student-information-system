275e8453375aeeaa41aa6023408114325e8f08ec
jacevihzalel <jacevihzalel@gmail.com>
2024-11-08 05:26:21 +0800

    Added backend functionality to receive data for a new student from the frontend

--- website/views.py
+++ website/views.py
@@ -1,9 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('route', __name__)
 
-@views.route('')
+@views.route('', methods=['GET', 'POST'])
 def students():
+    if request.method == 'POST':
+        id_number = request.form.get('id_number')
+        first_name = request.form.get('first_name')
+        last_name = request.form.get('last_name')
+        year_level = request.form.get('year_level')
+        course = request.form.get('course')
+        sex = request.form.get('sex')
+
     return render_template("students.html")
 
 @views.route('courses')
