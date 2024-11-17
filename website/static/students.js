7e14b9ab1f51d0f80b60a4e830aa92c38741c50f
jacevihzalel <jacevihzalel@gmail.com>
2024-11-18 07:30:38 +0800

    Added working prototype of error handling when receiving invalid values of student information

---
+++ website/static/students.js
@@ -0,0 +1,45 @@
+function handleFormSubmission(event) {
+  event.preventDefault();
+  
+  const form = event.target;
+  const formData = new FormData(form);
+  
+  fetch(window.location.href, {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.text())
+  .then(html => {
+    const tempDiv = document.createElement('div');
+    tempDiv.innerHTML = html;
+    const flashMessages = tempDiv.getElementsByClassName('alert');
+    
+    if (flashMessages.length > 0) {
+      form.reset();
+      
+      const modalFlashContainer = document.querySelector('#new_student_modal .modal-header');
+      const currentFlashMessages = Array.from(flashMessages).map(flash => flash.outerHTML).join('');
+      
+      const existingFlashes = modalFlashContainer.querySelectorAll('.alert');
+      existingFlashes.forEach(flash => flash.remove());
+      
+      modalFlashContainer.insertAdjacentHTML('afterend', currentFlashMessages);
+    } else {
+      const modal = bootstrap.Modal.getInstance(document.getElementById('new_student_modal'));
+      modal.hide();
+      location.reload();
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+document.addEventListener('click', function(e) {
+  if (e.target && e.target.classList.contains('btn-close')) {
+    const alert = e.target.closest('.alert');
+    if (alert) {
+      alert.remove();
+    }
+  }
+});
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, get_flashed_messages
 from flask_sqlalchemy import SQLAlchemy
 from .models import Students
 from . import db
@@ -16,8 +16,7 @@ def students():
         course = request.form.get('course')
         sex = request.form.get('sex')
 
-        """
-        if not id_number or not first_name or not last_name or not year_level or not course or not sex:
+        if not all([id_number, first_name, last_name, year_level, course, sex]):
             flash('All fields are required', 'error')
         elif not re.match(r'^\d{4}-\d{4}$', id_number):
             flash('ID Number must be in format XXXX-XXXX (e.g., 2024-0001)', category='error')
@@ -26,13 +25,11 @@ def students():
         elif re.search(r'\d', last_name):
             flash('Last name should not contain numbers', 'error')
         else:
-            flash('New student added successfully', 'error')
-        """
-
-        new_student = Students(id_number=id_number, last_name=last_name, first_name=first_name, year_level=year_level, course=course, sex=sex)
-        db.session.add(new_student)
-        db.session.commit()
-        
+            new_student = Students(id_number=id_number, last_name=last_name, first_name=first_name, year_level=year_level, course=course, sex=sex)
+            db.session.add(new_student)
+            db.session.commit()
+            flash('New student added successfully', 'success')
+            
     return render_template("students.html")
 
 @views.route('courses')
